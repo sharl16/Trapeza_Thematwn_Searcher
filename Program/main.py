badda4489120a235b99dd253f1e0441c41cf1f78
sharl16 <nickonlyforaccs@gmail.com>
2024-12-05 22:21:09 +0200

    1

--- Program/main.py
+++ Program/main.py
@@ -1,22 +1,21 @@
 import os
 import fitz  
 import re
+import requests
 
 list = []
+
+if not os.path.exists(r"PDFs\B"):
+     print("Required libraries were not found. Downloading..")
 
 def normalize_text(text):
     text = re.sub(r'\s+', ' ', text)  
     text = text.strip()  
     return text
 
-def index_pdf(folder_path, word_to_find):
-    word_to_find = normalize_text(word_to_find.lower())
-
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.pdf'):
-            file_path = os.path.join(folder_path, filename)
-            print(f"Searching in: {filename}")
-            with fitz.open(file_path) as pdf_document:
+def index_in_pdf(file_path, filename, word_to_find):
+    print(f"Searching in: {filename}")
+    with fitz.open(file_path) as pdf_document:
                 for page_num in range(pdf_document.page_count):
                     page = pdf_document.load_page(page_num)  
                     text = page.get_text()  
@@ -27,18 +26,26 @@ def index_pdf(folder_path, word_to_find):
                             list.insert(len(filename), filename)
                             break
 
+def index_frontend(folder_path, word_to_find):
+    word_to_find = normalize_text(word_to_find.lower())
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.pdf'):
+            file_path = os.path.join(folder_path, filename)
+            index_in_pdf(file_path, filename, word_to_find) 
+
 subject = input('Επιλογή Μαθήματος (ΦΥΣΙΚΗ ΘΕΤΙΚΗ/ ΜΑΘΗΜΑΤΙΚΑ ΘΕΤΙΚΗ/ ΑΛΓΕΒΡΑ/ ΓΛΩΣΣΑ)')
 print("Επιλέχθηκε: "+subject)
 segment = input('Επιλογή εκφώνησης')
 
 if subject == 'ΦΥΣΙΚΗ ΘΕΤΙΚΗ':
-    index_pdf(r'Mathimata\B\Fysikh', segment)
+    index_frontend(r'PDFs\B\Fysikh', segment)
 elif subject == "ΜΑΘΗΜΑΤΙΚΑ ΘΕΤΙΚΗ":
-    index_pdf(r'Mathimata\B\Math_Kateuth', segment)
+    index_frontend(r'Mathimata\B\Math_Kateuth', segment)
 elif subject == "ΑΛΓΕΒΡΑ":
-    index_pdf(r'Mathimata\B\Algebra', segment)
+    index_frontend(r'Mathimata\B\Algebra', segment)
 elif subject == "ΓΛΩΣΣΑ":
-    index_pdf(r'Mathimata\B\Glwssa', segment)
+    index_frontend(r'Mathimata\B\Glwssa', segment)
 else:
     print("Δεν υπάρχει το μάθημα: "+subject)
 
